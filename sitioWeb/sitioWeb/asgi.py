22469e4b5d284be26add8afded5f1d573a6f4400
Daniel Martinez Bonilla <mabo.daniel@gmail.com>
2020-07-05 15:20:42 -0600

    Cambio en el case

--- sitioWeb/sitioWeb/asgi.py
+++ sitioWeb/sitioWeb/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for SitioWeb project.
+ASGI config for sitioWeb project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SitioWeb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sitioWeb.settings')
 
 application = get_asgi_application()
