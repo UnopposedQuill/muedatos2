fa1f4fe03b561a8dea4df87ffa02371c67f0e802
Daniel Martinez Bonilla <mabo.daniel@gmail.com>
2020-07-22 16:29:11 -0600

    crear tablas mysql

---
+++ atencionCliente/creacionTablas.sql
@@ -0,0 +1,34 @@
+use AtencionCliente;
+
+Create table Cliente (
+	id int AUTO_INCREMENT primary key,
+    nombre varchar(20),
+    apellidos varchar(20),
+    fechaNacimiento date
+);
+
+create table Cupon (
+	id int auto_increment primary key,
+    idCliente int ,
+    idSucursal int,
+    fechaEntrega date,
+    dechaVencimiento date,
+    utilizado bit,
+    foreign key(idCliente) references Cliente(id)
+);
+
+create table Telefono (
+	id int auto_increment primary key,
+    idCliente int,
+    telefono varchar(16),
+    descripcion varchar(20),
+    foreign key(idCliente) references Cliente(id)
+);
+
+create table Correo (
+	id int auto_increment primary key,
+    idCliente int,
+    correo varchar(50),
+    descripcion varchar(20),
+    foreign key(idCliente) references Cliente(id)
+);
