d429e668e9e8e7be479c736495a6fbb701039f8c
David Espinoza <david1998es16@gmail.com>
2020-07-30 01:56:08 -0600

    Insertar diversos registros

---
+++ sucursal/columnas_metodo_pago_ABC.sql
@@ -0,0 +1,25 @@
+-- metodos de pago
+-- sucursal A
+insert into MetodoPago (idCliente, descripcion) values (7, 'efectivo');
+insert into MetodoPago (idCliente, descripcion) values (8, 'efectivo');
+insert into MetodoPago (idCliente, descripcion) values (9, 'efectivo');
+insert into MetodoPago (idCliente, descripcion) values (10, 'efectivo');
+insert into MetodoPago (idCliente, descripcion) values (10, 'MasterCard');
+insert into MetodoPago (idCliente, descripcion) values (11, 'efectivo');
+insert into MetodoPago (idCliente, descripcion) values (11, 'MasterCard');
+insert into MetodoPago (idCliente, descripcion) values (12, 'efectivo');
+insert into MetodoPago (idCliente, descripcion) values (12, 'MasterCard');
+-- sucursal B
+insert into MetodoPago (idCliente, descripcion) values (1, 'efectivo');
+insert into MetodoPago (idCliente, descripcion) values (2, 'efectivo');
+insert into MetodoPago (idCliente, descripcion) values (3, 'efectivo');
+insert into MetodoPago (idCliente, descripcion) values (3, 'Visa');
+insert into MetodoPago (idCliente, descripcion) values (4, 'efectivo');
+insert into MetodoPago (idCliente, descripcion) values (5, 'MasterCard');
+-- sucursal c
+insert into MetodoPago (idCliente, descripcion) values (1, 'efectivo');
+insert into MetodoPago (idCliente, descripcion) values (2, 'efectivo');
+insert into MetodoPago (idCliente, descripcion) values (3, 'efectivo');
+insert into MetodoPago (idCliente, descripcion) values (3, 'Visa');
+insert into MetodoPago (idCliente, descripcion) values (4, 'efectivo');
+insert into MetodoPago (idCliente, descripcion) values (5, 'MasterCard');
