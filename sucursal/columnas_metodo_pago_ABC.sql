2c334a70a54ab3ef1b12599ab18f35cd064195c6
Daniel Martinez Bonilla <mabo.daniel@gmail.com>
2020-08-07 16:37:03 -0600

    Un insert por tabla

--- sucursal/columnas_metodo_pago_ABC.sql
+++ sucursal/columnas_metodo_pago_ABC.sql
@@ -1,25 +1,25 @@
 -- metodos de pago
 -- sucursal A
-insert into MetodoPago (idCliente, descripcion) values (7, 'efectivo');
-insert into MetodoPago (idCliente, descripcion) values (8, 'efectivo');
-insert into MetodoPago (idCliente, descripcion) values (9, 'efectivo');
-insert into MetodoPago (idCliente, descripcion) values (10, 'efectivo');
-insert into MetodoPago (idCliente, descripcion) values (10, 'MasterCard');
-insert into MetodoPago (idCliente, descripcion) values (11, 'efectivo');
-insert into MetodoPago (idCliente, descripcion) values (11, 'MasterCard');
-insert into MetodoPago (idCliente, descripcion) values (12, 'efectivo');
-insert into MetodoPago (idCliente, descripcion) values (12, 'MasterCard');
+insert into MetodoPago (idCliente, descripcion) values (7, 'efectivo'),
+(8, 'efectivo'),
+(9, 'efectivo'),
+(10, 'efectivo'),
+(10, 'MasterCard'),
+(11, 'efectivo'),
+(11, 'MasterCard'),
+(12, 'efectivo'),
+(12, 'MasterCard');
 -- sucursal B
-insert into MetodoPago (idCliente, descripcion) values (1, 'efectivo');
-insert into MetodoPago (idCliente, descripcion) values (2, 'efectivo');
-insert into MetodoPago (idCliente, descripcion) values (3, 'efectivo');
-insert into MetodoPago (idCliente, descripcion) values (3, 'Visa');
-insert into MetodoPago (idCliente, descripcion) values (4, 'efectivo');
-insert into MetodoPago (idCliente, descripcion) values (5, 'MasterCard');
+insert into MetodoPago (idCliente, descripcion) values (1, 'efectivo'),
+(2, 'efectivo'),
+(3, 'efectivo'),
+(3, 'Visa'),
+(4, 'efectivo'),
+(5, 'MasterCard');
 -- sucursal c
-insert into MetodoPago (idCliente, descripcion) values (1, 'efectivo');
-insert into MetodoPago (idCliente, descripcion) values (2, 'efectivo');
-insert into MetodoPago (idCliente, descripcion) values (3, 'efectivo');
-insert into MetodoPago (idCliente, descripcion) values (3, 'Visa');
-insert into MetodoPago (idCliente, descripcion) values (4, 'efectivo');
-insert into MetodoPago (idCliente, descripcion) values (5, 'MasterCard');
+insert into MetodoPago (idCliente, descripcion) values (1, 'efectivo'),
+(2, 'efectivo'),
+(3, 'efectivo'),
+(3, 'Visa'),
+(4, 'efectivo'),
+(5, 'MasterCard');
--- sucursal/empleados_C.sql
+++ sucursal/empleados_C.sql
@@ -1,20 +1,20 @@
-insert into dbo.Empleado (nombre, apellidos, idPuesto, fechaContratacion, foto, salario) values ('Tonnie', 'Forrestill', 2, '2002-10-10', 'http://dummyimage.com/137x216.bmp/dddddd/000000', 56.3486);
-insert into dbo.Empleado (nombre, apellidos, idPuesto, fechaContratacion, foto, salario) values ('Lisbeth', 'McManus', 2, '2003-02-06', 'http://dummyimage.com/130x180.jpg/dddddd/000000', 57.5272);
-insert into dbo.Empleado (nombre, apellidos, idPuesto, fechaContratacion, foto, salario) values ('Benjamin', 'Walford', 3, '2002-11-21', 'http://dummyimage.com/131x162.bmp/dddddd/000000', 86.1615);
-insert into dbo.Empleado (nombre, apellidos, idPuesto, fechaContratacion, foto, salario) values ('Kimmie', 'Radish', 2, '2005-04-26', 'http://dummyimage.com/146x102.bmp/dddddd/000000', 79.6372);
-insert into dbo.Empleado (nombre, apellidos, idPuesto, fechaContratacion, foto, salario) values ('Tammie', 'McGarvey', 2, '2000-09-19', 'http://dummyimage.com/178x109.png/5fa2dd/ffffff', 71.3463);
-insert into dbo.Empleado (nombre, apellidos, idPuesto, fechaContratacion, foto, salario) values ('Munmro', 'Coopland', 3, '2004-09-17', 'http://dummyimage.com/174x111.jpg/ff4444/ffffff', 51.0014);
-insert into dbo.Empleado (nombre, apellidos, idPuesto, fechaContratacion, foto, salario) values ('Harley', 'Mellows', 2, '2003-05-27', 'http://dummyimage.com/108x172.bmp/dddddd/000000', 64.7786);
-insert into dbo.Empleado (nombre, apellidos, idPuesto, fechaContratacion, foto, salario) values ('Farrah', 'Boise', 2, '2002-01-22', 'http://dummyimage.com/214x148.bmp/5fa2dd/ffffff', 78.7242);
-insert into dbo.Empleado (nombre, apellidos, idPuesto, fechaContratacion, foto, salario) values ('Nanci', 'Jacklin', 1, '2000-06-12', 'http://dummyimage.com/157x180.bmp/ff4444/ffffff', 56.4892);
-insert into dbo.Empleado (nombre, apellidos, idPuesto, fechaContratacion, foto, salario) values ('Karlie', 'Ellison', 3, '2002-03-10', 'http://dummyimage.com/230x233.png/ff4444/ffffff', 76.2141);
-insert into dbo.Empleado (nombre, apellidos, idPuesto, fechaContratacion, foto, salario) values ('Kenyon', 'Drabble', 1, '2005-12-20', 'http://dummyimage.com/143x202.png/cc0000/ffffff', 62.9835);
-insert into dbo.Empleado (nombre, apellidos, idPuesto, fechaContratacion, foto, salario) values ('Erminie', 'Monksfield', 2, '2006-04-08', 'http://dummyimage.com/241x197.jpg/dddddd/000000', 62.4678);
-insert into dbo.Empleado (nombre, apellidos, idPuesto, fechaContratacion, foto, salario) values ('Ferguson', 'Sutton', 3, '2003-04-18', 'http://dummyimage.com/144x107.bmp/dddddd/000000', 82.2084);
-insert into dbo.Empleado (nombre, apellidos, idPuesto, fechaContratacion, foto, salario) values ('Caria', 'Whiteoak', 1, '2000-06-07', 'http://dummyimage.com/100x218.jpg/cc0000/ffffff', 61.175);
-insert into dbo.Empleado (nombre, apellidos, idPuesto, fechaContratacion, foto, salario) values ('Mar', 'Cumbridge', 1, '2006-03-18', 'http://dummyimage.com/230x188.bmp/ff4444/ffffff', 67.0984);
-insert into dbo.Empleado (nombre, apellidos, idPuesto, fechaContratacion, foto, salario) values ('Brett', 'Gerry', 3, '2004-11-01', 'http://dummyimage.com/105x132.png/dddddd/000000', 94.8187);
-insert into dbo.Empleado (nombre, apellidos, idPuesto, fechaContratacion, foto, salario) values ('Kathlin', 'Fritz', 1, '2006-05-30', 'http://dummyimage.com/121x126.jpg/ff4444/ffffff', 63.9435);
-insert into dbo.Empleado (nombre, apellidos, idPuesto, fechaContratacion, foto, salario) values ('Aeriela', 'Emor', 2, '2006-02-14', 'http://dummyimage.com/243x160.bmp/5fa2dd/ffffff', 95.2051);
-insert into dbo.Empleado (nombre, apellidos, idPuesto, fechaContratacion, foto, salario) values ('Carmen', 'Baumert', 1, '2000-09-13', 'http://dummyimage.com/188x230.bmp/5fa2dd/ffffff', 79.5908);
-insert into dbo.Empleado (nombre, apellidos, idPuesto, fechaContratacion, foto, salario) values ('Agnesse', 'Cescon', 2, '2004-08-18', 'http://dummyimage.com/108x170.png/cc0000/ffffff', 94.3788);
+insert into dbo.Empleado (nombre, apellidos, idPuesto, fechaContratacion, foto, salario) values ('Tonnie', 'Forrestill', 2, '2002-10-10', 'http://dummyimage.com/137x216.bmp/dddddd/000000', 56.3486),
+('Lisbeth', 'McManus', 2, '2003-02-06', 'http://dummyimage.com/130x180.jpg/dddddd/000000', 57.5272),
+('Benjamin', 'Walford', 3, '2002-11-21', 'http://dummyimage.com/131x162.bmp/dddddd/000000', 86.1615),
+('Kimmie', 'Radish', 2, '2005-04-26', 'http://dummyimage.com/146x102.bmp/dddddd/000000', 79.6372),
+('Tammie', 'McGarvey', 2, '2000-09-19', 'http://dummyimage.com/178x109.png/5fa2dd/ffffff', 71.3463),
+('Munmro', 'Coopland', 3, '2004-09-17', 'http://dummyimage.com/174x111.jpg/ff4444/ffffff', 51.0014),
+('Harley', 'Mellows', 2, '2003-05-27', 'http://dummyimage.com/108x172.bmp/dddddd/000000', 64.7786),
+('Farrah', 'Boise', 2, '2002-01-22', 'http://dummyimage.com/214x148.bmp/5fa2dd/ffffff', 78.7242),
+('Nanci', 'Jacklin', 1, '2000-06-12', 'http://dummyimage.com/157x180.bmp/ff4444/ffffff', 56.4892),
+('Karlie', 'Ellison', 3, '2002-03-10', 'http://dummyimage.com/230x233.png/ff4444/ffffff', 76.2141),
+('Kenyon', 'Drabble', 1, '2005-12-20', 'http://dummyimage.com/143x202.png/cc0000/ffffff', 62.9835),
+('Erminie', 'Monksfield', 2, '2006-04-08', 'http://dummyimage.com/241x197.jpg/dddddd/000000', 62.4678),
+('Ferguson', 'Sutton', 3, '2003-04-18', 'http://dummyimage.com/144x107.bmp/dddddd/000000', 82.2084),
+('Caria', 'Whiteoak', 1, '2000-06-07', 'http://dummyimage.com/100x218.jpg/cc0000/ffffff', 61.175),
+('Mar', 'Cumbridge', 1, '2006-03-18', 'http://dummyimage.com/230x188.bmp/ff4444/ffffff', 67.0984),
+('Brett', 'Gerry', 3, '2004-11-01', 'http://dummyimage.com/105x132.png/dddddd/000000', 94.8187),
+('Kathlin', 'Fritz', 1, '2006-05-30', 'http://dummyimage.com/121x126.jpg/ff4444/ffffff', 63.9435),
+('Aeriela', 'Emor', 2, '2006-02-14', 'http://dummyimage.com/243x160.bmp/5fa2dd/ffffff', 95.2051),
+('Carmen', 'Baumert', 1, '2000-09-13', 'http://dummyimage.com/188x230.bmp/5fa2dd/ffffff', 79.5908),
+('Agnesse', 'Cescon', 2, '2004-08-18', 'http://dummyimage.com/108x170.png/cc0000/ffffff', 94.3788);
