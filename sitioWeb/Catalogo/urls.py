34cb75f36accfc037165f7f45a4d1381a41a0773
erumabo <mabo.daniel@gmail.com>
2020-08-11 00:01:34 -0600

    Vista de sucursales disponibles ordenadas
    Vista de lista de productos

--- sitioWeb/Catalogo/urls.py
+++ sitioWeb/Catalogo/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('<int:uid>/', views.sucursales, name='sucursales'),
-    path('<int:uid>/<int:sid>', views.catalogo, name='catalogo'),
+    path('<int:uid>/<int:sid>/', views.catalogo, name='catalogo'),
     path('<int:uid>/<int:sid>/carro', views.carrito, name='carrito'),
 ]
--- sitioWeb/Catalogo/views.py
+++ sitioWeb/Catalogo/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.db import connection, connections
 
 # Create your views here.
 def sucursales(request,uid=0):
     ## EXEC [dbo].[spGetSucursales] uid
-    return render(request,'Catalogo/sucursales.html',{});
+    context = {'sucursales':[],'uid':uid}
+    with connections['taller'].cursor() as cursor: #coneccion a taller
+        cursor.execute("EXEC [dbo].[spGetSucursales] %s",[uid]);
+        context['sucursales'] = [row for row in cursor.fetchall()];
+    return render(request,'Catalogo/sucursales.html',context);
 
 def catalogo(request,uid=0,sid=0):
     ## EXEC [dbo].[spGetProductos] sid
-    return render(request,'Catalogo/catalogo.html',{});
+    context = {'productos':[],'uid':uid,'sid':sid}
+    with connections['taller'].cursor() as cursor: #coneccion a taller
+        cursor.execute("EXEC [dbo].[spGetProductos] %s",[sid]);
+        context['productos'] = [row for row in cursor.fetchall()];
+    return render(request,'Catalogo/catalogo.html',context);
 
 def carrito(request,uid=0,sid=0):
     ## POST, solo muestra el carrito y permite eliminar productos de la lista
-    return render(request,'Catalogo/carro.html',{});
+    if request.method == 'POST':
+        context = {'carrito':request.POST['carrito'],'uid':uid,'sid':sid};
+        return render(request,'Catalogo/carro.html',{});
+    else:
+        return redirect('sucursales/'+uid+'/',uid=uid,permanent=True);
