7c69a214a289173dd2aa5e9763352738dbf93a20
erumabo <mabo.daniel@gmail.com>
2020-08-10 23:01:58 -0600

    arreglo rapido que en el commit anterior le di restore y me volé la mitad del trabajo

--- sitioWeb/Catalogo/views.py
+++ sitioWeb/Catalogo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.db import connection, connections
 
 # Create your views here.
+def sucursales(request,uid=0):
+    ## EXEC [dbo].[spGetSucursales] uid
+    return render(request,'Catalogo/sucursales.html',{});
+
+def catalogo(request,uid=0,sid=0):
+    ## EXEC [dbo].[spGetProductos] sid
+    return render(request,'Catalogo/catalogo.html',{});
+
+def carrito(request,uid=0,sid=0):
+    ## POST, solo muestra el carrito y permite eliminar productos de la lista
+    return render(request,'Catalogo/carro.html',{});
--- sitioWeb/Compra/views.py
+++ sitioWeb/Compra/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def compra(request,uid=0,sid=0):
+    ## EXEC [dbo].[spGetSucursales] uid
+    return render(request,'Compra/compra.html',{});
+
+def facturar(request,uid=0,sid=0):
+    ## EXEC [dbo].[spGetProductos] sid
+    return render(request,'Compra/factuar.html',{});
