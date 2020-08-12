4bc210a67641ec0264537ea581040cf101bfa641
David Espinoza <david1998es16@gmail.com>
2020-08-12 13:18:44 -0600

    Eliminacion de archivos duplicados

--- sucursal/columnas_VentaB.sql
+++
@@ -1,3 +0,0 @@
-insert into dbo.Venta (idEmpleado, idCliente, idMetodoPago, fechaVenta) values (7, 5, 4, '2020-01-28');
-insert into dbo.Venta (idEmpleado, idCliente, idMetodoPago, fechaVenta) values (13, 1, 4, '2020-05-14');
-insert into dbo.Venta (idEmpleado, idCliente, idMetodoPago, fechaVenta) values (8, 1, 5, '2019-10-25');
