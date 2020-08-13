4a735dc92e8613db74bd65993ce5724300c8971f
UnopposedQuill <darklightbeing@gmail.com>
2020-08-13 12:04:05 -0600

    Proyecto de Pycharm, cambio en particion.sql, gitignore
    Se crea un proyecto para pycharm para que sea más sencillo de migrar de una computadora a otra, se mejora la descripción en particion.sql, se genera un .gitignore para eliminar los archivos precompilados de python

--- taller/10_particion.sql
+++ taller/10_particion.sql
@@ -7,5 +7,6 @@ AS RANGE LEFT FOR VALUES (1, 2);
 CREATE PARTITION SCHEME [sucursal] AS PARTITION [sucursal] TO ([SucursalA], [SucursalB], [SucursalC])
 
 -- por motivos que escapan a mi entendimiento hay que crear el filegroup y files usando el _gui_
--- "click derecho y propiedades en la BD, files"
+-- "click derecho y propiedades en la BD, files; luego filegroups"
+-- "hay que crear 3 files, y 3 filegroups, cada uno con el nombre SucursalX"
 -- "click derecho y storage en la tabla, partition"
